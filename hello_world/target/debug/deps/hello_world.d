b034fa98c3c3029be179dc0ba9778d317f7470f8
sayed-maheen <sayedmaheen28@gmail.com>
2023-07-11 08:49:31 +0400

    RS0003: removed  all "target" dir from git

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-F:\FLAGSHIP\RUST\RUSTPROG\master_rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-F:\FLAGSHIP\RUST\RUSTPROG\master_rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-F:\FLAGSHIP\RUST\RUSTPROG\master_rust\hello_world\target\debug\hello_world.exe: F:\FLAGSHIP\RUST\RUSTPROG\master_rust\hello_world\src\main.rs
